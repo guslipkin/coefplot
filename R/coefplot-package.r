312b9134d17bad6a033e06acccf19a96506e0a50
Gus Lipkin <gus.lipkin@gmail.com>
2024-04-29 16:05:00 -0400

    Update for modern rlang::.data pronoun

--- R/coefplot-package.r
+++ R/coefplot-package.r
@@ -3,15 +3,10 @@
 #' Provides an S3 generic method for plotting coefficients from a model so it can be extended to other model types.
 #'
 #' Currently, methods are available for lm, glm and rxLinMod objects.
-#'
-# @import plyr ggplot2 reshape2
-#' @docType package
-#' @name coefplot
-#' @aliases coefplot-package
+#' @keywords internal
+"_PACKAGE"
+## usethis namespace: start
+#' @importFrom rlang .data
+#' @importFrom rlang .env
+## usethis namespace: end
 NULL
-
-
-## quiets concerns of R CMD check re: the .data's that appear in pipelines 
-## because dplyr doesn't offer a way to use arrange with Standard evaluation
-
-if(getRversion() >= "2.15.1")  utils::globalVariables(c(".data"))
